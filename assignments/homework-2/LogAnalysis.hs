7393c626be81161cc304028f2efdf51b15ebeee9
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-10-11 10:11:00 +0700

    done homework 2 exercise 2

--- assignments/homework-2/LogAnalysis.hs
+++ assignments/homework-2/LogAnalysis.hs
@@ -18,11 +18,32 @@ parseMessage :: String -> LogMessage
 parseMessage msgStr =
   case msgStr of
     "" -> Unknown msgStr
-    ('E' : s) -> let msgList = split ' ' msgStr in LogMessage (Error (read (msgList !! 1) :: Int)) (read (msgList !! 2) :: Int) (join " " (take 3 msgList))
-    ('I' : s) -> let msgList = split ' ' msgStr in LogMessage Info (read (msgList !! 1) :: Int) (join " " (take 3 msgList))
-    ('W' : s) -> let msgList = split ' ' msgStr in LogMessage Warning (read (msgList !! 1) :: Int) (join " " (take 3 msgList))
+    ('E' : s) -> 
+      let msgList = split ' ' msgStr 
+      in LogMessage (Error (read (msgList !! 1) :: Int)) (read (msgList !! 2) :: Int) (join " " (drop 3 msgList))
+    ('I' : s) -> 
+      let msgList = split ' ' msgStr 
+      in LogMessage Info (read (msgList !! 1) :: Int) (join " " (drop 2 msgList))
+    ('W' : s) -> 
+      let msgList = split ' ' msgStr 
+      in LogMessage Warning (read (msgList !! 1) :: Int) (join " " (drop 2 msgList))
 
 parse :: String -> [LogMessage]
 parse "" = []
 parse logs =
   [parseMessage log | log <- split '\n' logs]
+
+getTimeStamp :: LogMessage -> TimeStamp
+getTimeStamp (LogMessage _ timestamp _) = timestamp
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert log Leaf = Node Leaf log Leaf
+insert log tree = 
+  let
+    treeTimeStamp = getTimeStamp ((\(Node _ log _) -> log) tree)
+    logTimeStamp = getTimeStamp log
+  in
+    if logTimeStamp > treeTimeStamp 
+    then Node Leaf log tree
+    else Node tree log Leaf
